5bd49d893b64185f1bb42c472ba213bad6e35ed3
Matt <mhgarry92@gmail.com>
2023-07-31 04:19:02 -0400

    add logic to relate upvotes to posts in db

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,8 @@
 from datetime import datetime 
 from app.db import Base
+from .Vote import Vote # importing our Vote model to create a relationship between our Post and Vote models and add the ability to count the number of votes a post has
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, column_property # adding a column property to our Post model to count the number of votes a post has
 
 class Post(Base): # create a Post class that makes a table called posts
     __tablename__ = 'posts'
@@ -11,6 +12,10 @@ class Post(Base): # create a Post class that makes a table called posts
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id) # logic to count the number of votes a post has and the func.count() function to count the number of votes a post has
+    )
 
     user = relationship('User') # create a relationship between our Post and User models
     comments = relationship('Comment', cascade='all,delete') # create a relationship between our Post and Comment models and cascade all deletes to our comments table
+    votes = relationship('Vote', cascade='all,delete') # create a relationship between our Post and Vote models and cascade all deletes to our votes table
