3949c31c99be067bbf02112f8e9729d8c97a4430
Matt <mhgarry92@gmail.com>
2023-07-30 23:06:27 -0400

    commented code comparing to express framework to reference

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 
 def create_app(test_config=None):
-  # set up app config
+  # set up app config and create instance of Flask
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
 
+  # create route for hello world equivalent of app.route('/hello, (req, res) => {
+  # res.send('Hello, world!;)}) in express
   @app.route('/hello')
   def hello():
     return 'Hello, world!'
